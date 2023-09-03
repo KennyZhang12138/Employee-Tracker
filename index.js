6df2ed9a5ef04663118b6ac899fa7f4621cc1896
Kenny Zhang <dzhang33@CougarNet.UH.EDU>
2023-09-02 22:00:01 -0500

    add basic SELECT functions

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 const connection = require("./config/connection");
 const inquirer = require("inquirer");
+const cTable = require("console.table");
 
 connection.connect((error) => {
   if (error) throw error;
-  console.log(`====================================================================================`);
-  console.log(`|                                                                                  |`);
-  console.log(`|                                                                                  |`);
-  console.log("|                                Employee Tracker                                  |");
-  console.log(`|                                                                                  |`);
-  console.log(`|                                                                                  |`);
-  console.log("|                                                      Created By: Dunjiang Zhang  |");
-  console.log(`|                                                                                  |`);
-  console.log(`====================================================================================`);
+  console.log(
+    `====================================================================================`
+  );
+  console.log(
+    `|                                                                                  |`
+  );
+  console.log(
+    `|                                                                                  |`
+  );
+  console.log(
+    "|                                Employee Tracker                                  |"
+  );
+  console.log(
+    `|                                                                                  |`
+  );
+  console.log(
+    `|                                                                                  |`
+  );
+  console.log(
+    "|                                                      Created By: Dunjiang Zhang  |"
+  );
+  console.log(
+    `|                                                                                  |`
+  );
+  console.log(
+    `====================================================================================`
+  );
   options();
 });
 
@@ -61,8 +80,68 @@ const options = () => {
         case "Remove Employee":
           removeEmployee();
         case "Exit":
-          console.log("Exiting Database.")
+          console.log("Exiting Database.");
           connection.end();
       }
     });
 };
+
+const viewAllDepartments = () => {
+  const sql = `SELECT department.id AS ID, department.name AS Department FROM department`;
+  connection.query(sql, (error, response) => {
+    if (error) throw error;
+    console.log(`====================================================================================`);
+    console.log(`|                             All Departments:                                     |`);
+    console.table(response);
+    console.log(`====================================================================================`);
+    options();
+  });
+};
+
+const viewAllRoles = () => {
+  const sql = `SELECT role.id AS ID, role.title AS Title, role.salary AS Salary, department.name AS Department 
+  FROM role INNER JOIN department ON role.department_id = department.id`;
+  connection.query(sql, (error, response) => {
+    if (error) throw error;
+    console.log(`=====================================================================================`);
+    console.log(`|                                    All Roles:                                     |`);
+    console.table(response);
+    console.log(`=====================================================================================`);
+    options();
+  });
+};
+
+const viewAllEmployees = () => {
+  const sql = `SELECT employee.id, 
+                  employee.first_name AS FirstName, 
+                  employee.last_name AS LastName, 
+                  role.title AS Title, 
+                  department.name AS 'Department', 
+                  role.salary,
+                  employee.manager_id AS Manager
+                  FROM employee, role, department 
+                  WHERE department.id = role.department_id 
+                  AND role.id = employee.role_id`;
+  connection.query(sql, (error, response) => {
+    if (error) throw error;
+    console.log(`=====================================================================================`);
+    console.log(`|                                    All Employee:                                  |`);
+    console.table(response);
+    console.log(`=====================================================================================`);
+    options();
+  });
+};
+
+const addDepartment = () => {};
+
+const addRole = () => {};
+
+const addEmployee = () => {};
+
+const updateEmployeeRole = () => {};
+
+const removeDepartment = () => {};
+
+const removeRole = () => {};
+
+const removeEmployee = () => {};
